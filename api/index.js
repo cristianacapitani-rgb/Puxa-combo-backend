44b5fde750c686f3dbd1083e6cbdc1a4a6dc422d
cristianacapitani-rgb <cristiana.capitani@gmail.com>
2025-12-17 19:32:33 -0300

    Update index.js

--- api/index.js
+++ api/index.js
@@ -1,3 +1,10 @@
+import { createClient } from "@supabase/supabase-js";
+
+const supabase = createClient(
+  process.env.SUPABASE_URL,
+  process.env.SUPABASE_SERVICE_ROLE_KEY
+);
+
 app.post("/api/webhooks/mercadopago", async (req, res) => {
   try {
     const paymentId =
